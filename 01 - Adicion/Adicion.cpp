5781eb2302f5066959d466804bb525871fdbc6b2
GastonNNieva <38082880+GastonNNieva@users.noreply.github.com>
2018-05-03 17:54:33 -0300

    Add files via upload

---
+++ 01 - Adicion/Adicion.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+	std::cout << "ingrese a y luego b y recibira el resultado de la suma de ambos\n"; 
+	int a;
+	std::cin >> a;
+	int b;
+	std::cin >> b;
+	std::cout << a + b;
+}
